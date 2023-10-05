908be6a5dc375fc7c7d4e6a1a8f84e183f470c93
JJMoraless <jjmorales.dev@gmail.com>
2023-10-05 05:47:00 -0500

    feat: :sparkles: añadido middlewares de uso comuno para errores, roles

--- libs/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./cloudinary.js";
-export * from "./multer.js";
-export * from "./sharp.js";
--- libs/sharp.js
+++
@@ -1,7 +0,0 @@
-import sharp from "sharp";
-
-export const resizeImg = async (filePath, fileName, size = 300) => {
-  const pathOptimized = `./uploads/optimize/${fileName}`;
-  const resultOptimize = await sharp(filePath).resize(size).toFile(pathOptimized);
-  return { pathOptimized, resultOptimize };
-};
